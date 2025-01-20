43554612486b0d8b2db3535e00d1b864bf2d67b2
ruichengeng <ruichengeng@gmail.com>
2025-01-20 17:40:42 -0500

    Update Python Fitting Ex 1 RGeng.py

--- Python Fitting Exercise/Python Fitting Ex 1 RGeng.py
+++ Python Fitting Exercise/Python Fitting Ex 1 RGeng.py
@@ -13,15 +13,18 @@ import numpy as np
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 
-#Imports the data from file named co2_annmean_mlo for the annual average of daily measurement of atmospheric co2 from 1960 to 2023 from the top of Mauna Loa in Hawaii.
+#Imports and reads the data from file named co2_annmean_mlo for the annual average of daily measurement of atmospheric co2 from 1960 to 2023 from the top of Mauna Loa in Hawaii.
 #Below: We skips 44 rows so that the first data entry is from 1959 aka the first row containing data
 #We also separated the data category based on separation character ',' 
 #Then we unpacked these into different arrays of data according to the type (i.e. years separated from co2 level separated from the uncertainty)
+#year is an array with each element as the year in unit of year
+#mean is an array with each element as the mean of the co2 level in a given year in unit of ppm
+#unc is an array with each element as the uncertainty of the mean co2 level in a given year, also in unit of ppm
 year, mean, unc = np.loadtxt("co2_annmean_mlo.csv", delimiter = ',', skiprows=44, unpack=True)
 
 
-#Here we define the different types of fitting models, the independent variable is x_val
-#We will use curve_fit function later to estimate/fit the values of the coefficients A, B, and C where they are applicable
+#Here we define the different types of fitting models, the independent variable is x_val and coefficients A, B, and C where they are applicable
+#We will use curve_fit function later to estimate/fit the values of the above-mentioned coefficients 
 
 #Linear fitting model using the same style as the formula f(x)=ax+b
 def linear_model(x_val, A, B):
@@ -47,48 +50,117 @@ def exponential_model(x_val, A, B, C):
     return A*np.exp(B*(x_val-1959))+C
 
 
+#Using the curve_fit function for each of the above models
+
+#Define new variables for the linear fitting model so that we can only account for the last 20 years of data.
+lin_year = year[-20:]
+lin_mean = mean[-20:]
+lin_unc = unc[-20:]
+#Linear model curve fitting using the new variables
+lin_popt, lin_pcov = curve_fit(linear_model, lin_year, lin_mean)
+
+#Quadratic model curve fitting
+quad_popt, quad_pcov = curve_fit(quadratic_model, year, mean)
+
+#Power model curve fitting
+pow_popt, pow_pcov = curve_fit(power_model, year, mean)
+
+#Exponential model curve fitting
+#Here we are providing the initial value estimating value of A = 1, B = almost 0, C = 200 (this is still below the smallest co2 level in the given data set)
+exp_popt, exp_pcov = curve_fit(exponential_model, year, mean, p0=(1, 1e-6, 200))
 
 
+#Model values and their residuals
+#Data returned by the linear model
+lin_model_data = lin_year*0
+lin_residual = lin_year*0
+for l in range(len(lin_year)):
+    lin_model_data[l]=linear_model(lin_year[l], lin_popt[0], lin_popt[1])
+    lin_residual[l]=lin_model_data[l]-lin_mean[l]
 
-#################################
-plt.plot(year, mean, label='Data')
-#yerr_up= mean+unc
-#yerr_down= mean-unc
-#yerr=[-1*unc, unc]
-unc2=unc*50
-plt.errorbar(year, mean, yerr=unc, capsize=1, fmt = 'none', ecolor = "blue")
+#Initializing the arrays of the expected values from the models after curve fit
+quad_model_data = year*0
+quad_residual = year*0
 
-lin_year=year[-20:]
-print(lin_year)
+pow_model_data = year*0
+pow_residual = year*0
 
-popt, pcov = curve_fit(power_model, year, mean)
-popt2, pcov2 = curve_fit(exponential_model, year, mean, p0=(1, 1e-6, 200))
+exp_model_data = year*0
+exp_residual = year*0
 
-plt.plot(year, power_model(year, popt[0], popt[1], popt[2]), label = "Quadratic Curve Fit")
+#Calculating the expected values and finding their respective residuals
+for i in range (len(year)):
+    #First the expected values
+    quad_model_data[i]=quadratic_model(year[i], quad_popt[0], quad_popt[1], quad_popt[2])
+    pow_model_data[i]=power_model(year[i], pow_popt[0], pow_popt[1], pow_popt[2])
+    exp_model_data[i]=exponential_model(year[i], exp_popt[0], exp_popt[1], exp_popt[2])
+    #Then calculate the residuals
+    quad_residual[i] = quad_model_data[i]-mean[i]
+    pow_residual[i] = pow_model_data[i]-mean[i]
+    exp_residual[i] = exp_model_data[i]-mean[i]
 
-plt.plot(year, exponential_model(year, popt2[0], popt2[1], popt2[2]), label = "Exponential Curve Fit")
-plt.xlabel("x values")
-plt.ylabel("y values")
+
+#Plotting the datas and models and the residues
+#Plotting the linear model, the respective original data set, and the residuals
+plt.figure(figsize = (8, 16))
+
+#First subplot corresponding to the original data set and the linear model's fitting.
+plt.subplot(2, 1, 1)
+plt.plot(lin_year, lin_mean, label = "Data (last 20 years)")
+plt.plot(lin_year, linear_model(lin_year, lin_popt[0], lin_popt[1]), label = "Linear Model Curve Fit", color="blue")
+plt.errorbar(lin_year, lin_mean, yerr=lin_unc, capsize=1, fmt = 'none', ecolor = "blue")
+plt.xlabel("Year")
+plt.ylabel(r'$CO_2\:Level\:(in\:unit\:of\:ppm)$')
+plt.xticks(np.arange(2004, 2023, step = 2))
 plt.legend()
+plt.title("Mean CO$_2$ level with linear model curve fitting")
+
+#Second subplot for the residuals, with a newly defined variable lin_zero_err as the line where the residual is 0.
+lin_zero_err = np.zeros(len(lin_year))
+plt.subplot(2, 1, 2)
+plt.plot(lin_year, lin_zero_err)
+plt.plot(lin_year, lin_residual, label = "Residual of the linear model versus actual data")
+
+
+
+
+
+
+#missing residual error bars
+
+
+
+
+
+
+
+plt.xlabel("Year")
+plt.ylabel(r'$Error\:of\:CO_2\:Level\:in\:the\:linear\:model\:(in\:unit\:of\:ppm)$')
+plt.xticks(np.arange(2004, 2023, step = 2))
+plt.legend()
+plt.title("Residuals from the linear model")
 plt.show()
 
-print ("A value:", popt2[0])
-print ("B value:", popt2[1])
-print ("C value:", popt2[2])
 
-print (power_model(2060, popt[0], popt[1], popt[2]))
-print (exponential_model(2060, popt2[0], popt2[1], popt2[2]))
 
-#perr = np.sqrt(np.diag(pcov))
 
-#plt.plot(year, quad_quiz(year, popt[0], popt[1], popt[2]), label = "Quadratic Curve Fit")
-#plt.xlabel("x values")
-#plt.ylabel("y values")
-#plt.legend()
-#plt.show()
+# print ("A value:", popt2[0])
+# print ("B value:", popt2[1])
+# print ("C value:", popt2[2])
 
-#print ("A value:", popt[0])
-#print ("B value:", popt[1])
-#print ("C value:", popt[2])
+# print (power_model(2060, popt[0], popt[1], popt[2]))
+# print (exponential_model(2060, popt2[0], popt2[1], popt2[2]))
 
-#print (quad_quiz(2060, popt[0], popt[1], popt[2]))
+# #perr = np.sqrt(np.diag(pcov))
+
+# #plt.plot(year, quad_quiz(year, popt[0], popt[1], popt[2]), label = "Quadratic Curve Fit")
+# #plt.xlabel("x values")
+# #plt.ylabel("y values")
+# #plt.legend()
+# #plt.show()
+
+# #print ("A value:", popt[0])
+# #print ("B value:", popt[1])
+# #print ("C value:", popt[2])
+
+# #print (quad_quiz(2060, popt[0], popt[1], popt[2]))
