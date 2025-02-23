b43cb1fdddc896d8334a592f6353f67324386af8
ruichengeng <ruichengeng@gmail.com>
2025-02-23 07:04:48 -0500

    Thermal voltage and leakage current

--- Circuit/Circuit_Diode.py
+++ Circuit/Circuit_Diode.py
@@ -14,6 +14,7 @@ Due February 23rd, 2025
 
 #Necessary modules
 import numpy as np 
+import scipy
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt 
 
@@ -73,61 +74,9 @@ print("Exponential Model: ", reduced_chi2_exp)
 print("Shockley Model: ", reduced_chi2_shock)
 
 
+#Thermal Voltage Calculation
+thermal_volt_lower = 1.0/(shock_popt[0])
+thermal_volt_higher = 1.0/(2.0*shock_popt[0])
 
-# ### LOG MODEL ERROR PROPAGATION ###
-# dlogI_dV = log_popt[1] / (forward_volt + log_popt[0])  # ∂(log I) / ∂V
-# dlogI_dI = 1 / -1*amp[0]  # ∂(log I) / ∂I
-
-# log_model_unc = np.sqrt((dlogI_dV * forward_volt_unc) ** 2 + (dlogI_dI * forward_amp_unc) ** 2)
-
-# ### EXPONENTIAL MODEL ERROR PROPAGATION ###
-# c, b = exp_popt[2], exp_popt[1]  # Extract fitted parameters
-# exp_model = exponential_model(volt, *exp_popt)  # Compute model values
-# dexp_dV = exp_model * c * np.log(b)  # ∂I/∂V
-# dexp_dI = np.ones_like(amp)  # ∂I/∂I
-
-# exp_model_unc = np.sqrt((dexp_dV * volt_unc) ** 2 + (dexp_dI * amp_unc) ** 2)
-
-# ### SHOCKLEY MODEL ERROR PROPAGATION ###
-# a = shock_popt[0]  # Extract fitted parameter
-# shock_model = shockley_model(volt, a)  # Compute model values
-# dshock_dV = -amp[0] * a * np.exp(a * volt)  # ∂I/∂V
-# dshock_dI = np.ones_like(amp)  # ∂I/∂I
-
-# shock_model_unc = np.sqrt((dshock_dV * volt_unc) ** 2 + (dshock_dI * amp_unc) ** 2)
-
-# ### PRINT RESULTS ###
-# print("Log Model Uncertainty:", log_model_unc)
-# print("Exponential Model Uncertainty:", exp_model_unc)
-# print("Shockley Model Uncertainty:", shock_model_unc)
-
-
-# # Number of data points
-# N = len(amp)
-# N_log = len(forward_amp)
-# # Number of parameters for each model
-# p_log = len(log_popt)
-# p_exp = len(exp_popt)
-# p_shock = len(shock_popt)
-
-# # Degrees of freedom
-# dof_log = N_log - p_log
-# dof_exp = N - p_exp
-# dof_shock = N - p_shock
-
-# # Compute residuals (difference between observed and model values)
-# log_residuals = (forward_amp - np.exp(linear_log_model(forward_volt, *log_popt))) / log_model_unc
-# exp_residuals = (amp - exponential_model(volt, *exp_popt)) / exp_model_unc
-# shock_residuals = (amp - shockley_model(volt, *shock_popt)) / shock_model_unc
-
-# # Compute chi-squared values
-# chi2_log = np.sum(log_residuals**2) / dof_log
-# chi2_exp = np.sum(exp_residuals**2) / dof_exp
-# chi2_shock = np.sum(shock_residuals**2) / dof_shock
-
-# # Print results
-# print("Reduced Chi-Squared Values:")
-# print("Log Model: ", chi2_log)
-# print("Exponential Model: ", chi2_exp)
-# print("Shockley Model: ", chi2_shock)
-
+print("Given that the ideality factor η varies between 1 and 2, then our thermal voltage varies between: ", thermal_volt_lower*1000.0, "mV and ", thermal_volt_higher*1000.0, "mV")
+print("The leakage current is approximately: ", -1.0*amp[0], "mA or approximately: ", -1000.0*amp[0], "μA")
