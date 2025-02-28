f2293bf1a713e8d7871a433f700a44749e60197f
ruichengeng <ruichengeng@gmail.com>
2025-02-28 15:31:19 -0500

    Added double slit data, attempted to clean the data but still require more fixing

--- Interference and Diffraction/Interference_and_Diffraction.py
+++ Interference and Diffraction/Interference_and_Diffraction.py
@@ -18,10 +18,14 @@ import scipy
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt 
 
-position, intensity = np.loadtxt("single_slit_data_pos_100x.txt", 
+position, intensity = np.loadtxt("double_slit_data_100x.txt", 
                                                   delimiter = '\t', 
                                                   skiprows=2, unpack=True)
 
+
+count = int(position.size/3)
+position = position[count:-count]
+intensity = intensity[count:-count]
 
 max_index = 0   
 max_intensity = 0.0
@@ -31,15 +35,24 @@ for i in range(len(intensity)):
         max_index = i
         max_intensity = intensity[i]
 
-def id_model(x_val, a, b, c, d):
-    return a*np.sin(b*x_val+c)/(x_val+d)
+def id_model(x_val, a, b, c):
+    #return a*np.sin(b*x_val+c)/(x_val+d)
+    return a*np.sin(b*x_val)/(c*x_val)
+
+def temp_model(x_val, a, b, c, d, e):
+    return a*((np.sin(b*x_val)/(d*x_val))**2)*(np.cos(e*x_val)**2)
     
 
 # def id_model(x_val, a, b, c):
 #     return a*np.sin(b*(x_val-position[max_index])/(c*(x_val-position[max_index])))
 
-popt, pcov = curve_fit(id_model, position, intensity, maxfev = 100000)
-    
+# popt, pcov = curve_fit(id_model, position, intensity, maxfev = 100000)
+popt, pcov = curve_fit(temp_model, position, intensity, maxfev = 100000)
+
+
+plt.figure(figsize=(50, 25))
 plt.plot(position, intensity)
-plt.plot(position, id_model(position, popt[0], popt[1], popt[2], popt[3]), color = "red")
+# plt.plot(position, id_model(position, popt[0], popt[1], popt[2], popt[3]), color = "red")
+plt.plot(position, temp_model(position, popt[0], popt[1], popt[2], popt[3], popt[4]), color = "red")
+
 # plt.plot(position, id_model(position, popt[0], popt[1], popt[2]), color = "red")
