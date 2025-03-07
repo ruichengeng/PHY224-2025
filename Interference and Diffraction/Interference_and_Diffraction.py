d8e029e61d9ece88e0cec963ab58c1f51f25dfd1
ruichengeng <ruichengeng@gmail.com>
2025-03-07 15:22:53 -0500

    Refined slit model, much more accurate

--- Interference and Diffraction/Interference_and_Diffraction.py
+++ Interference and Diffraction/Interference_and_Diffraction.py
@@ -18,6 +18,9 @@ import scipy
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt 
 
+# position, intensity = np.loadtxt("double_slit_a0.04_d0.5_100x.txt", 
+#                                                   delimiter = '\t', 
+#                                                   skiprows=2, unpack=True)
 position, intensity = np.loadtxt("double_slit_data_100x.txt", 
                                                   delimiter = '\t', 
                                                   skiprows=2, unpack=True)
@@ -32,9 +35,12 @@ max_intensity = 0.0
 max_position = 0.0
 
 wavelength = 650e-9    # 650 nm in meters
+wheel_sensor_dist = 0.515 #distance between the light sensor and the slit wheel
 
 d = 0.25e-3  # Slit separation: 0.25 mm in meters
-w = 0.04e-3  # Slit width: 0.04 mm in meters
+a = 0.04e-3  # Slit width: 0.04 mm in meters
+# d = 0.5e-3  # Slit separation: 0.25 mm in meters
+# a = 0.04e-3  # Slit width: 0.04 mm in meters
 
 for i in range(len(intensity)):
     if intensity[i] > max_intensity:
@@ -56,18 +62,30 @@ def temp_model(x_val, a, b):
     phi = np.sin(b)*np.pi*a/wavelength
     return max_intensity*((np.sin(phi)/(phi))**2)
 
-def double_slit_model(x_val, I0, k):
-    theta = np.arcsin((x_val - position[max_index]) * k)
+# def double_slit_model(x_val, I0, k):
+#     theta = np.arcsin((x_val - position[max_index]) * k)
     
-    phi = np.pi * w * np.sin(theta) / wavelength
-    phi = np.where(phi == 0, 1e-9, phi)    
-    beta = np.pi * d * np.sin(theta) / wavelength
+#     phi = np.pi * w * np.sin(theta) / wavelength
+#     phi = np.where(phi == 0, 1e-9, phi)    
+#     beta = np.pi * d * np.sin(theta) / wavelength
 
-    single_slit = (np.sinc(phi / np.pi)) ** 2
-    double_slit = np.cos(beta) ** 2
+#     single_slit = (np.sinc(phi / np.pi)) ** 2
+#     double_slit = np.cos(beta) ** 2
 
-    return I0 * single_slit * double_slit
+#     return I0 * single_slit * double_slit
 
+def double_slit_model(x_val, a, b, c, d):
+    #theta = np.arcsin(wavelength / a)
+    sin_theta = (x_val - max_position)/(np.sqrt(wheel_sensor_dist**2 + (x_val - max_position)**2))
+    
+    phi = np.pi * a * sin_theta / wavelength
+    beta = np.pi * b * sin_theta / wavelength
+
+    single_slit = (np.sinc(phi)) ** 2
+    double_slit = (np.cos(beta)) ** 2
+    offset_slit = (np.sinc(phi)) ** 2
+
+    return d* max_intensity * (single_slit * double_slit) - c* offset_slit
 
 # def id_model(x_val, a, b, c):
 #     return a*np.sin(b*(x_val-position[max_index])/(c*(x_val-position[max_index])))
@@ -77,7 +95,10 @@ def double_slit_model(x_val, I0, k):
 # popt, pcov = curve_fit(temp_model, position, intensity, p0=(400.0, 400.0), maxfev = 100000)
 #popt, pcov = curve_fit(temp_model, position, intensity, p0=(0.0004, 1.0), maxfev = 100000)
 
-popt, pcov = curve_fit(double_slit_model, position, intensity, p0=(max_intensity, 1.0), maxfev = 10000)
+popt, pcov = curve_fit(double_slit_model, position, intensity, p0=(a, d, 0.4, 0.5))
+
+
+print("a value is: ", wavelength/(max_position*popt[1]))
 
 plt.figure(figsize=(15, 8))
 plt.errorbar(position, intensity, fmt='o', color = "red", label = "Measured Data")
@@ -113,7 +134,7 @@ residual_pos = residual_pos[peaks]
 def residual_sin_model(x_val, I0, k, is_Single = False):
     theta = np.arcsin((x_val - position[max_index]) * k)
     
-    phi = np.pi * w * np.sin(theta) / wavelength
+    phi = np.pi * a * np.sin(theta) / wavelength
     phi = np.where(phi == 0, 1e-9, phi)    
     beta = np.pi * d * np.sin(theta) / wavelength
 
@@ -150,21 +171,22 @@ for r in range(len(position)):
     if position[r] >= residual_pos[0] and position[r] <= residual_pos[-1]:
         new_measured_data[r]-= residual_sin_model(position[r], *res_popt[:-1], True)
 
-plt.figure(figsize=(15, 8))
-plt.errorbar(position, new_measured_data, fmt='o', color = "red", label = "Measured Data")
+# plt.figure(figsize=(15, 8))
+# # plt.errorbar(position, new_measured_data, fmt='o', color = "red", label = "Measured Data")
+# plt.errorbar(position, intensity, fmt='o', color = "red", label = "Measured Data")
 
-plt.plot(position, double_slit_model(position, *popt), color = "blue", label = "Fitted Data")
+# plt.plot(position, double_slit_model(position, *popt)-residual_sin_model(position, *res_popt[:-1], True), color = "blue", label = "Fitted Data")
 
-# plt.plot(position, id_model(position, popt[0], popt[1], popt[2]), color = "red")
-plt.xlabel("Position (m)")
-plt.ylabel("Intensity (V)")
-plt.legend()
+# # plt.plot(position, id_model(position, popt[0], popt[1], popt[2]), color = "red")
+# plt.xlabel("Position (m)")
+# plt.ylabel("Intensity (V)")
+# plt.legend()
 
-plt.show()
+# plt.show()
 
 
 
-wheel_sensor_dist = 0.515
+
 
 max_residual_intensity = 0.0
 max_residual_pos = 0.0
@@ -188,4 +210,4 @@ central_to_min_dist = np.abs(max_residual_pos - first_min_residual_pos)
 
 residual_slit_width = wavelength / (central_to_min_dist/(np.sqrt(wheel_sensor_dist**2 + central_to_min_dist**2)))
 
-print("Slit distance is ", residual_slit_width)
+print("Slit width is ", residual_slit_width)
