24878e74046cc2a9cbc52bee6e8a20724ac8b471
ruichengeng <ruichengeng@gmail.com>
2025-03-14 13:14:49 -0400

    Initial blank python script for interferometer lab

---
+++ Inteferometer/Interferometer.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 14 13:12:00 2025
+
+Turki Almansoori
+Rui (Richard) Chen Geng Li
+
+Code created for PHY224 Free Choice Lab: Interferometer
+Prof. Sergio De La Barrera
+Due March 23rd, 2025
+"""
+
+#Necessary modules
+import numpy as np 
+import scipy
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt 
+
+
+
