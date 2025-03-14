665c04362916b5fbacd9d831f9c3d464b667cd37
ruichengeng <ruichengeng@gmail.com>
2025-03-14 17:02:54 -0400

    Initial models for interferometer lab, and temporary data for wavelength prediction (actual data require re-measurement due to equipment malfunction)

--- Inteferometer/Interferometer.py
+++ Inteferometer/Interferometer.py
@@ -16,5 +16,60 @@ import scipy
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt 
 
+#Data imports
+#calibration interferometer data for python
+reading, dN, reading_unc, dN_unc = np.loadtxt("calibration interferometer data for python.csv", 
+                                                  delimiter = ',', 
+                                                  skiprows=1, unpack=True)
+
+#Prediction Model
+#Wavelength
+def deltaN(x_val, a):
+    return x_val*(2.0/a)
+
+#Thermal Coefficient of Aluminium
 
 
+dN_Per_dx = np.zeros(dN.size)
+
+for n in range(1, len(dN)):
+    dN_Per_dx[n]=dN[n]+dN_Per_dx[n-1]
+    
+#Curve_fit
+
+
+#Plotting
+plt.errorbar(reading, dN_Per_dx, xerr=reading_unc, yerr=dN_unc)
+
+
+
+###################### Index of Refraction ################################
+
+#Reading Data
+
+
+#Variables
+gamma = 650e-9 #Temp for the wavelength
+
+#Prediction Model
+#Index of Refraction
+def index_refraction(x_val, a, b):
+    #a is t for thickness
+    #b is theta
+    return (((x_val*gamma/(2.0*a))+np.cos(b)-1)**2 + np.sin(b)**2)/(2.0*(1.0-np.cos(b)-(x_val*gamma/(2.0*a))))
+
+
+
+
+###################### Thermal Expansion of Aluminium ################################
+
+#Reading Data
+
+
+#Variables
+L0=1.0 #Temp, length at base temperature
+
+#Prediction Model
+#Thermal Expansion
+def thermal_expansion(x_val, a):
+    return L0*np.exp()**(a*x_val)
