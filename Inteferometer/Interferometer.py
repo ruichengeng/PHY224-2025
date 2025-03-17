b1c590a3eef736ff16e812a206b769aecb6dad74
ruichengeng <ruichengeng@gmail.com>
2025-03-17 17:48:26 -0400

    New measurement data, curve fitted to predict wavelength

--- Inteferometer/Interferometer.py
+++ Inteferometer/Interferometer.py
@@ -17,8 +17,7 @@ from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt 
 
 #Data imports
-#calibration interferometer data for python
-reading, dN, reading_unc, dN_unc = np.loadtxt("Interferometer_Fringe_Count2.csv", 
+reading, dN, reading_unc, dN_unc = np.loadtxt("Final_Wavelength_data.csv", 
                                                   delimiter = ',', 
                                                   skiprows=1, unpack=True)
 
@@ -28,17 +27,17 @@ def deltaN(x_val, a):
     return x_val*(2.0/a)
 
 dN_Per_dx = np.zeros(dN.size)
-
-# for n in range(1, len(dN)):
-#     dN_Per_dx[n]=dN[n]+dN_Per_dx[n-1]
+dN_Per_dx[0]=reading[0]
+for n in range(1, len(dN)):
+    dN_Per_dx[n]=dN[n]+dN_Per_dx[n-1]
     
 #Curve_fit
-# cf_popt, cf_pcov = curve_fit(deltaN, reading, dN_Per_dx, p0=(0.5), sigma=dN_unc, absolute_sigma = True)
-cf_popt, cf_pcov = curve_fit(deltaN, reading, dN, p0=(0.5), sigma=dN_unc, absolute_sigma = True)
+cf_popt, cf_pcov = curve_fit(deltaN, reading, dN_Per_dx, p0=(0.5), sigma=dN_unc, absolute_sigma = True)
+# cf_popt, cf_pcov = curve_fit(deltaN, reading, dN, p0=(0.5), sigma=dN_unc, absolute_sigma = True)
 
 #Plotting
-plt.errorbar(reading, dN, xerr=reading_unc, yerr=dN_unc, label = "Measured Data")
-# plt.errorbar(reading, dN_Per_dx, xerr=reading_unc, yerr=dN_unc, label = "Measured Data")
+# plt.errorbar(reading, dN, xerr=reading_unc, yerr=dN_unc, label = "Measured Data")
+plt.errorbar(reading, dN_Per_dx, xerr=reading_unc, yerr=dN_unc, label = "Measured Data")
 plt.plot(reading, deltaN(reading, *cf_popt), color = "red", label="Prediction Data")
 plt.legend()
 
