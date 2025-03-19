d797ecc68192c44ab69418bce09efd6d7b3c7f00
ruichengeng <ruichengeng@gmail.com>
2025-03-19 16:31:24 -0400

    Changing the script to adjust new measured data for wavelength prediction

--- Inteferometer/Interferometer.py
+++ Inteferometer/Interferometer.py
@@ -17,7 +17,7 @@ from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt 
 
 #Data imports
-reading, dN, reading_unc, dN_unc = np.loadtxt("Final_Wavelength_data.csv", 
+reading, dN, reading_unc, dN_unc = np.loadtxt("Final_Wavelength_data - Copy.csv", 
                                                   delimiter = ',', 
                                                   skiprows=1, unpack=True)
 
@@ -32,34 +32,71 @@ for n in range(1, len(dN)):
     dN_Per_dx[n]=dN[n]+dN_Per_dx[n-1]
     
 #Curve_fit
-cf_popt, cf_pcov = curve_fit(deltaN, reading, dN_Per_dx, p0=(0.5), sigma=dN_unc, absolute_sigma = True)
+cf_popt, cf_pcov = curve_fit(deltaN, reading, dN_Per_dx, p0=(0.57), sigma=dN_unc, absolute_sigma = True)
 # cf_popt, cf_pcov = curve_fit(deltaN, reading, dN, p0=(0.5), sigma=dN_unc, absolute_sigma = True)
 
 #Plotting
 # plt.errorbar(reading, dN, xerr=reading_unc, yerr=dN_unc, label = "Measured Data")
 plt.errorbar(reading, dN_Per_dx, xerr=reading_unc, yerr=dN_unc, label = "Measured Data")
 plt.plot(reading, deltaN(reading, *cf_popt), color = "red", label="Prediction Data")
+plt.plot(reading, deltaN(reading, 0.57)-10.0, color = "black", label="temp")
 plt.legend()
-
+plt.show()
 
 #Printing the predicted wavelength
 print("Predicted Wavelength = ", cf_popt[0]*1000.0, "nm ± ", np.sqrt(cf_pcov[0][0])*1000.0, "nm")
 
+position_std = np.sqrt(np.sum((reading-np.mean(reading))**2)/reading.size)
+#position_std += reading_unc*2.0
+#position_std = reading_unc
+reduced_chi2 = np.sum((reading-deltaN(reading, *cf_popt))**2/position_std**2) /(reading.size - cf_popt.size)
+print ("The Reduced Chi Square Value is: ", reduced_chi2)
+
+#position_std = np.sqrt(np.sum((position-np.mean(position))**2)/position.size)
+reduced_chi2 = np.sum((reading-deltaN(reading, 0.57)-10.0)**2/position_std**2) /(reading.size - cf_popt.size)
+print ("The Reduced Chi Square Value is: ", reduced_chi2)
 ###################### Index of Refraction ################################
 
 #Reading Data
+ir_reading, ir_min, ir_dN, ir_min_unc = np.loadtxt("Index of Refraction Data.csv", 
+                                                  delimiter = ',', 
+                                                  skiprows=1, unpack=True)
+
+ir_dN=ir_dN[::-1]
+
+ir_dN_Per_dx = np.zeros(ir_dN.size)
+ir_dN_Per_dx[0]=ir_reading[0]
+for n in range(1, len(ir_dN)):
+    ir_dN_Per_dx[n]=ir_dN[n]+ir_dN_Per_dx[n-1]
 
 
+ir_reading += ir_min*0.01
+
+ir_reading_init = ir_reading[0]
+ir_reading *= -1.0
+ir_reading += ir_reading_init
 #Variables
 gamma = 650e-9 #Temp for the wavelength
+thickness = 7e-6 #mm converted to nm
 
 #Prediction Model
 #Index of Refraction
-def index_refraction(x_val, a, b):
-    #a is t for thickness
-    #b is theta
-    return (((x_val*gamma/(2.0*a))+np.cos(b)-1)**2 + np.sin(b)**2)/(2.0*(1.0-np.cos(b)-(x_val*gamma/(2.0*a))))
+# def index_refraction(x_val, a, b):
+#     #a is t for thickness
+#     #b is theta
+#     return (((x_val*gamma/(2.0*a))+np.cos(b)-1)**2 + np.sin(b)**2)/(2.0*(1.0-np.cos(b)-(x_val*gamma/(2.0*a))))
 
+
+def index_refraction_2(x_val, a):
+    return (thickness/cf_popt[0])*(x_val**2)*(1.0-(1.0/a))
+
+ir_popt, ir_pcov = curve_fit(index_refraction_2, ir_reading[1:], ir_dN_Per_dx[1:], sigma = np.ones(ir_reading[1:].size)*0.5, absolute_sigma = True)
+
+
+plt.errorbar(ir_reading, ir_dN_Per_dx, xerr=ir_min_unc*0.01, color = "red", label = "Index of Refraction Measurement")
+plt.plot(ir_reading, index_refraction_2(ir_reading, *ir_popt), color = "blue", label = "Prediction")
+
+plt.legend()
 
 
 
