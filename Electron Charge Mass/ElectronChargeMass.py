75777919f088a7c5c9d1bb601339d32c33e796c6
ruichengeng <ruichengeng@gmail.com>
2025-03-31 16:42:20 -0400

    Added data with uncertainty calculation and residual plots

--- Electron Charge Mass/ElectronChargeMass.py
+++ Electron Charge Mass/ElectronChargeMass.py
@@ -26,19 +26,23 @@ k_char = (1.0/np.sqrt(2.0))*((4.0/5.0)**(3.0/2.0))*scipy.constants.mu_0*n/R #Cha
 
 #Data reading
 #Constant Current data
-cc_current, cc_voltage, cc_ps_volt, cc_diameter = np.loadtxt("Constant_Current_data.csv", 
+cc_current, cc_voltage, cc_ps_volt, cc_diameter, cc_current_unc, cc_voltage_unc, cc_ps_volt_unc, cc_diameter_unc = np.loadtxt("Constant_Current_data (w unc).csv", 
                                                   delimiter = ',', 
                                                   skiprows=1, unpack=True)
 cc_current = np.abs(cc_current)
 
 #Eliminating the last point to correct for very small voltage
-cc_current = cc_current[:-1]
-cc_voltage = cc_voltage[:-1]
-cc_ps_volt = cc_ps_volt[:-1]
-cc_diameter = cc_diameter[:-1]
+cc_current = cc_current[:-2]
+cc_voltage = cc_voltage[:-2]
+cc_ps_volt = cc_ps_volt[:-2]
+cc_diameter = cc_diameter[:-2]
+cc_current_unc = cc_current_unc[:-2]
+cc_voltage_unc = cc_voltage_unc[:-2]
+cc_ps_volt_unc = cc_ps_volt_unc[:-2]
+cc_diameter_unc = cc_diameter_unc[:-2]
 
 #Constant Voltage data
-cv_current, cv_voltage, cv_ps_volt, cv_diameter = np.loadtxt("Constant_Voltage_data.csv", 
+cv_current, cv_voltage, cv_ps_volt, cv_diameter, cv_current_unc, cv_voltage_unc, cv_ps_volt_unc, cv_diameter_unc = np.loadtxt("Constant_Voltage_data (w unc).csv", 
                                                   delimiter = ',', 
                                                   skiprows=1, unpack=True)
 
@@ -51,13 +55,14 @@ I = (np.max(cc_current)+np.min(cc_current))/2.0 #Used for constant current fitti
 
 #B_c values based on constant voltage data
 Bc = k_char*cv_current*np.sqrt(2)
+Bc_unc = k_char*cv_current_unc*np.sqrt(2)
 
 #Magnetic Field Bc Prediction Model
 def magnetic_fit_model(x_val, a, b):
     return a*(1.0/x_val) - b #Where b is B_e
 
 #Curve fitting for the magnetic field to obtain B_e
-b_popt, b_pcov = curve_fit(magnetic_fit_model, cv_diameter/2.0, Bc)
+b_popt, b_pcov = curve_fit(magnetic_fit_model, cv_diameter/2.0, Bc, sigma = Bc_unc, absolute_sigma = True)
 
 #Calculation of I_0
 I0 = b_popt[1]/k_char
@@ -71,29 +76,72 @@ def const_Voltage_model(x_val, a):
     return a/(x_val+1.0/np.sqrt(2)*I0)
 
 #Curve fitting for the constant voltage
-cv_popt, cv_pcov = curve_fit(const_Voltage_model, cv_current, cv_diameter/2.0)
+cv_popt, cv_pcov = curve_fit(const_Voltage_model, cv_current, cv_diameter/2.0, sigma = cv_diameter_unc/2.0, absolute_sigma = True)
 
 #Curve fitting for the constant current
-cc_popt, cc_pcov = curve_fit(const_Current_model, cc_voltage, cc_diameter/2.0)
+cc_popt, cc_pcov = curve_fit(const_Current_model, cc_voltage, cc_diameter/2.0, sigma = cc_diameter_unc/2.0, absolute_sigma = True)
 
 
-#Temp
-plt.errorbar(cc_voltage, (cc_diameter/2.0), color = "red", fmt = 'o')
-plt.plot(cc_voltage, const_Current_model(cc_voltage, *cc_popt), color = "green")
-plt.title("Constant Current")
+#Plotting constant current
+plt.figure(figsize = (8, 12))
+#Prediction plot
+plt.subplot(2, 1, 1)
+plt.errorbar(cc_voltage, (cc_diameter/2.0), xerr = cc_voltage_unc, yerr = cc_diameter_unc/2.0, color = "red", fmt = 'o', label = "Measured Data")
+plt.plot(cc_voltage, const_Current_model(cc_voltage, *cc_popt), color = "green", label = "Model Prediction")
+plt.title("Constant Current Prediction Model")
+plt.xlabel("Voltage(V)")
+plt.ylabel("Radius (cm)")
+plt.legend()
+
+#Residual calculation
+cc_prediction = const_Current_model(cc_voltage, *cc_popt)
+cc_residual = cc_diameter/2.0 - cc_prediction
+
+#Residual plot
+plt.subplot(2, 1, 2)
+plt.plot(cc_voltage, np.zeros(cc_voltage.size), color = "blue", label = "Zero residual reference line")
+plt.errorbar(cc_voltage, cc_residual, xerr = cc_voltage_unc, yerr = np.sqrt(cc_diameter_unc**2 + np.sqrt(cc_pcov[0][0])**2), color = "red", fmt = 'o', label = "Residual between measured and predicted data")
+plt.title("Residual of the constant current model")
+plt.xlabel("Voltage(V)")
+plt.ylabel("Error: Radius (cm)")
+
+plt.legend()
 plt.show()
-plt.errorbar(cv_current, (cv_diameter/2.0), color = "red", fmt = 'o')
-plt.plot(cv_current, const_Voltage_model(cv_current, *cv_popt), color = "green")
-plt.title("Constant Voltage")
+
+#Plotting constant voltage
+plt.figure(figsize = (8, 12))
+#Prediction plot
+plt.subplot(2, 1, 1)
+plt.errorbar(cv_current, (cv_diameter/2.0), xerr = cv_current_unc, yerr = cv_diameter_unc/2.0, color = "red", fmt = 'o', label = "Measured Data")
+plt.plot(cv_current, const_Voltage_model(cv_current, *cv_popt), color = "green", label = "Model Prediction")
+plt.title("Constant Voltage Prediction Model")
+plt.xlabel("Current (A)")
+plt.ylabel("Radius (cm)")
+plt.legend()
+
+#Residual calculation
+cv_prediction = const_Voltage_model(cv_current, *cv_popt)
+cv_residual = cv_diameter/2.0 - cv_prediction
+
+#Residual plot
+plt.subplot(2, 1, 2)
+plt.plot(cv_current, np.zeros(cv_voltage.size), color = "blue", label = "Zero residual reference line")
+plt.errorbar(cv_current, cv_residual, xerr = cv_current_unc, yerr = np.sqrt(cv_diameter_unc**2 + np.sqrt(cv_pcov[0][0])**2), color = "red", fmt = 'o', label = "Residual between measured and predicted data")
+plt.title("Residual of the constant voltage model")
+plt.xlabel("Current (A)")
+plt.ylabel("Error: Radius (cm)")
+
+plt.legend()
+plt.show()
 
 
 #Printing estimated charge to mass ratio
 #Via constant current
 cc_a_inv = 1.0/cc_popt[0]
 cc_a_inv /= (k_char*(I+ I0/np.sqrt(2)))
-print("Via constant current, the charge to mass ratio is: ", cc_a_inv**2, " C/kg")
+print("Via constant current, the charge to mass ratio is: ", cc_a_inv**2, " C/kg ±")
 
 #Via constant voltage
 cv_a_inv = 1.0/cv_popt[0]
 cv_a_inv*=(np.sqrt(deltaV)/k_char)
-print("Via constant voltage, the charge to mass ratio is: ", cv_a_inv**2, " C/kg")
+print("Via constant voltage, the charge to mass ratio is: ", cv_a_inv**2, " C/kg ±")
